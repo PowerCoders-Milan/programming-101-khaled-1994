d01f292bb60b9f91f357eef7b448bc8b3116f2f4
khaled sidawi <khaledassa123@gmail.com>
2021-02-19 12:29:12 +0100

    i added my code

--- 01-Blockly/blockly.js
+++ 01-Blockly/blockly.js
@@ -1,1 +1,22 @@
 //Insert level 9 code below
+while (notDone()) {
+    moveForward();
+    if (isPathForward()) {
+      turnLeft();
+    } else {
+      turnLeft();
+    }
+    if (isPathRight()) {
+      turnRight();
+    }
+  }
+  while (notDone()) {
+    if (isPathLeft()) {
+      turnLeft();
+    }
+    if (isPathForward()) {
+      moveForward();
+    } else {
+      turnRight();
+    }
+  }
